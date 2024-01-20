cb074c54809e9cc608d659f08c276c1b5f4edbee
Vanh020104 <114908216+Vanh020104@users.noreply.github.com>
2024-01-20 20:18:28 +0700

    update

---
+++ OgainShop/Controllers/AdminController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OgainShop.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: /<controller>/
+
+        // Order Management
+        public IActionResult order()
+        {
+            return View("OrderManagement/order");
+        }
+
+        // Customer Management
+        public IActionResult customer()
+        {
+            return View("CustomerManagement/customer");
+        }
+
+        // Product Management
+        public IActionResult product()
+        {
+            return View("ProductManagement/product");
+        }
+
+        public IActionResult addProduct()
+        {
+            return View("ProductManagement/addProduct");
+        }
+        public IActionResult editProduct()
+        {
+            return View("ProductManagement/editProduct");
+        }
+
+        // Dashboard
+        public IActionResult dashboard()
+        {
+            return View("DashboardAdmin/dashboard");
+        }
+
+    }
+}
